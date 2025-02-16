f262f92cf845848350980fa7e88d85ad400d3838
김도형 <mrdos@naver.com>
2025-02-16 23:58:39 +0900

    Initial commit

---
+++ src/main/java/himedia/hbgoguma/HbgogumaserverApplication.java
@@ -0,0 +1,13 @@
+package himedia.hbgoguma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbgogumaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbgogumaserverApplication.class, args);
+	}
+
+}
