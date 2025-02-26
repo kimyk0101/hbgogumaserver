b9cba8d95d39a401b7dedd0192643f7e071e2db6
김도형 <mrdos@naver.com>
2025-02-26 15:10:47 +0900

    [도형] 신고데이터 변수명 수정

--- src/main/java/himedia/hbgoguma/repository/vo/GogumaReport.java
+++ src/main/java/himedia/hbgoguma/repository/vo/GogumaReport.java
@@ -17,7 +17,7 @@ public class GogumaReport {
 	private Long rid;			// 신고글 번호
 	private Long uid;			// 신고한 유저번호
 	private Long pid;			// 신고당한 게시글 번호
-	private Integer reportId;	// 신고 사유
-	private Boolean isConfirm;	// 신고처리 완료 여부
-	private Date reportDate;	// 신고 일시
+	private Integer report_id;	// 신고 사유
+	private Boolean is_confirmed;	// 신고처리 완료 여부
+	private Date upd_date;	// 신고 일시
 }
