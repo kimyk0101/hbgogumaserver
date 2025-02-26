dd5d2eefba2f208466414cd72b078c567efd5cbf
김도형 <mrdos@naver.com>
2025-02-26 19:49:41 +0900

    [도형] 리뷰 기능 정상화

--- src/main/java/himedia/hbgoguma/repository/vo/GogumaReviewHistory.java
+++ src/main/java/himedia/hbgoguma/repository/vo/GogumaReviewHistory.java
@@ -15,10 +15,10 @@ import lombok.ToString;
 @ToString
 public class GogumaReviewHistory {
 	private Long hid;				// 구매 후기글 고유번호
-	private Long sendUid;			// 구매 후기글 작성자 유저번호
-	private Long receiveUid;		// 구매 게시글 올렸던 작성자 유저번호
-	private String reviewContent;	// 구매 후기글 내용
-	private Float reviewPoint;		// 판매자 대상 평점
-	private Integer rewardPoint;	// 후기글 작성 보상 포인트
-	private Date reviewUpdate;		// 후기글 작성 시간
+	private Long buyer_uid;			// 구매 후기글 작성자 유저번호
+	private Long seller_uid;		// 구매 게시글 올렸던 작성자 유저번호
+	private String review_content;	// 구매 후기글 내용
+	private Float review_point;		// 판매자 대상 평점
+	private Integer rewardpoint;	// 후기글 작성 보상 포인트
+	private Date upd_date;		// 후기글 작성 시간
 }
